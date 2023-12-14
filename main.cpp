f5ed500cd748a30fe598ec69769e60a848561cab
eo025075 <ecem.ozbiyik@ozu.edu.tr>
2023-12-14 15:52:42 +0300

    Added classes from Olcay teacher github.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by eo025075 on 12/14/2023.
+//
+
+#include "main.h"
+#include <iostream>
+
+
+int main() {
+    std:: cout << "Ecem" << std:: endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by eo025075 on 12/14/2023.
+//
+
+#ifndef GRAPH_PUZZLE_GAME_ECEMOZBIYIK_MAIN_H
+#define GRAPH_PUZZLE_GAME_ECEMOZBIYIK_MAIN_H
+
+namespace std {
+
+    class main {
+
+    };
+
+} // std
+
+#endif //GRAPH_PUZZLE_GAME_ECEMOZBIYIK_MAIN_H
